d09939472242ced2d2c6735eb600d9f63da871de
Yves NAHIMANA <yvesnahimana1@gmail.com>
2026-06-03 11:46:33 +0200

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+ baseURL: 'https://budget-tracker-production-6d82.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
